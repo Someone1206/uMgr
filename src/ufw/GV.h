95890014dfb9e732c2b1b2369a3a64acadb1a8bf
Someone <sangitabiswas0606@gmail.com>
2021-10-04 20:23:52 +0530

    Improved Docs

--- src/ufw/GV.h
+++ src/ufw/GV.h
@@ -8,16 +8,25 @@
 #include <unistd.h>
 #endif // _WIN32
 
+// a collection of all the global variables to be used
 namespace GV
 {
-    extern std::string getU_F_name();
+    /// <summary>
+    /// return the user folder name and updates the user folder path
+    /// </summary>
+    std::string getU_F_name();
 
     namespace consts
     {
+        // stores the folder and file separator
         extern char fsep;
+        // stores the the path of the user's home dir
         extern std::filesystem::path uPaf;
+        // stores the user folder name
         extern std::string uName;
+        // stores the path where the setting, pwd and others are stored in the user home dir
         extern std::string c_app_data;
+        // stores the path where the user stores their data
         extern std::string user_data_folder;
 
     } // namespace consts
