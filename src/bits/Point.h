831ee9bb5da284b66eacf14d600e1c80691bed1c
Someone <sangitabiswas0606@gmail.com>
2021-10-20 23:01:13 +0530

    Fixed + and -

--- src/bits/Point.h
+++ src/bits/Point.h
@@ -7,32 +7,36 @@ public:
 	int x = 0, y = 0;
 	Point(int _x = CW_USEDEFAULT, int _y = CW_USEDEFAULT);
 
-	Point operator+(Point& pt);
-	Point operator-(Point& pt);
+	Point operator+(const Point& pt);
+	Point operator-(const Point& pt);
 
 	~Point();
 };
 
-Point::Point(int _x, int _y)
+inline Point::Point(int _x, int _y)
 {
+	x = _x;
+	y = _y;
 }
 
-inline Point Point::operator+(Point& pt)
+inline Point Point::operator+(const Point& pt)
 {
-	pt.x = x + pt.x;
-	pt.y = y + pt.y;
-	return pt;
+	Point res;
+	res.x = x + pt.x;
+	res.y = y + pt.y;
+	return res;
 }
 
-inline Point Point::operator-(Point& pt)
+inline Point Point::operator-(const Point& pt)
 {
-	pt.x = x - pt.x;
-	pt.y = y - pt.y;
-	return pt;
+	Point res;
+	res.x = x - pt.x;
+	res.y = y - pt.y;
+	return res;
 }
 
-Point::~Point()
+inline Point::~Point()
 {
-	// watch anime?
+	// watch anime.
 }
 
