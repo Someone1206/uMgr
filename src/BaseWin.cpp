984563e741e77a72fe52cd9e607e74b8f42c242a
Someone <sangitabiswas0606@gmail.com>
2021-10-21 20:36:39 +0530

    Base Window

--- src/BaseWin.cpp
+++ src/BaseWin.cpp
@@ -1,1 +1,19 @@
 #include "BaseWin.h"
+
+void BaseWin::Show(bool _show)
+{
+	if (_show)
+		ShowWindow(hwnd, SW_SHOW);
+	else
+		ShowWindow(hwnd, SW_HIDE);
+}
+
+void BaseWin::Enable(bool _enable)
+{
+	EnableWindow(hwnd, _enable);
+}
+
+void BaseWin::Destroy()
+{
+	DestroyWindow(hwnd);
+}
--- src/BaseWin.h
+++ src/BaseWin.h
@@ -1,5 +1,13 @@
 #pragma once
+#include "Bits.h"
+
 class BaseWin
 {
+public:
+	HWND hwnd;
+
+	void Show(bool _show = true);
+	void Enable(bool _enable = true);
+	void Destroy();
 };
 
