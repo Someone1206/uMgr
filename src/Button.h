5210113b21427ca126fc3507475a4da8d39aba71
Someone <sangitabiswas0606@gmail.com>
2021-10-27 09:31:45 +0530

    Compressed to 1 file

--- src/Button.h
+++ src/Button.h
@@ -15,3 +15,15 @@ public:
     { /**/ }
 };
 
+Button::Button(HWND _parent, int id, LPCSTR txt, const Point& pos, const Size& size, int styles)
+    :BaseWin(pos, size, _parent), text(txt)
+{
+    hwnd = CreateWindowEx(
+        0,
+        "buTTon", text, styles,
+        pos.x, pos.y, size.x, size.y,
+        parent, (HMENU)id, GetModuleHandle(nullptr), nullptr
+    );
+
+    Show();
+}
